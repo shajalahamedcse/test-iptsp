f0d9e5acf246ba0c91ff7f7458834810f83f16d8
shajalAhamed <shajalahamedcse@gmail.com>
2020-11-29 14:01:24 +0600

    Added basic config

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route("/api/v1/obd_response",methods=["POST"])
+def obd_response():
+    data = request.data
+    print(data)
+    return {"msg":"Hello"}
+
+@app.route("/api/v1/alive")
+def alive():
+    return {"msg": "alive"}
+if __name__ == "__main__":
+    app.run()
---
+++ readme.md
@@ -0,0 +1,2 @@
+    
+    ssh -i dreadfulRat5f969nJj0.pem ubuntu@13.235.74.116
